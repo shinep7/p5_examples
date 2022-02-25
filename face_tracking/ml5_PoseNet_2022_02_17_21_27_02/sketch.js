ef1185b7ea916b7a1cd532fafdf579f90e7d893c
shinepark7 <shinepark22@marlborough.org>
2022-02-25 09:30:14 -0800

    Update sketch.js

--- face_tracking/ml5_PoseNet_2022_02_17_21_27_02/sketch.js
+++ face_tracking/ml5_PoseNet_2022_02_17_21_27_02/sketch.js
@@ -89,6 +89,6 @@ function drawCircles(){
   for(let k = 0; k < width; k++){
     colorMode(RGB, (k+1) * width * 10);
     fill(millis()%((k+1) * width * 10));
-    ellipse(random(width), random(height), 50, 50);
+    ellipse(random(k*width), random(height), 50, 50);
   }
 }
