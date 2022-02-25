182fc48983a29fe71b7f03171387f68ba8eba853
shinepark7 <shinepark22@marlborough.org>
2022-02-25 08:53:13 -0800

    Update sketch.js

--- face_tracking/ml5_PoseNet_2022_02_17_21_27_02/sketch.js
+++ face_tracking/ml5_PoseNet_2022_02_17_21_27_02/sketch.js
@@ -36,6 +36,14 @@ function draw() {
   background(0);
   image(video, 0, 0, width, height);
 
+  for (var x = 0; x < width; x += width / 10) {
+		for (var y = 0; y < height; y += height / 10) {
+			stroke(0);
+			strokeWeight(1);
+			line(x, 0, x, height);
+			line(0, y, width, y);
+		}
+	}
   // We can call both functions to draw all keypoints and the skeletons
   drawKeypoints();
   drawSkeleton();
